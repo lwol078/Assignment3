e2d1b5bc84447433087043fbb17cb91d16e548b6
Luke <lwol078@aucklanduni.ac.nz>
2014-09-17 18:43:32 +1200

    menus are fun

--- vamix/GUI/MainFrame.java
+++ vamix/GUI/MainFrame.java
@@ -8,7 +8,9 @@ import javax.swing.SwingUtilities;
 
 public class MainFrame extends JFrame
 {
-	private JPanel panel;
+	private JMenuBar menuBar;
+    private JMenu menu1,menu2;
+    private JMenuItem item1,item2;
 
 	public MainFrame()
 	{
@@ -17,7 +19,24 @@ public class MainFrame extends JFrame
         setSize(1050, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-        panel = new JPanel();
-        this.add(panel);
+        menuBar = new JMenuBar();
+
+        menu1 = new JMenu("Menu1");
+        menu1.getAccessibleContext().setAccessibleDescription(
+            "The only menu in this program that has menu items");
+        menuBar.add(menu1);
+
+        menu2 = new JMenu("Menu2");
+         menu1.getAccessibleContext().setAccessibleDescription(
+            "Look, a submenu!");
+        menu1.add(menu2);
+
+        item1 = new JMenuItem("Look, an item!");
+        menu1.add(item1);
+
+        item2 = new JMenuItem("How exciting, I'm in a submenu!");
+        menu2.add(item2);
+
+        setJMenuBar(menuBar);
 	}
 }
